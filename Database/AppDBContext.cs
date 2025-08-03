f1096720625016ec937ea60b3a7e0bfc354793b9
igorugor <igor18052003@gmail.com>
2025-08-03 22:18:07 +0300

    clear unneeded code

--- Database/AppDBContext.cs
+++ Database/AppDBContext.cs
@@ -5,6 +5,5 @@ namespace BirthdayTracker.Database;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    public DbSet<Todo> Todos { get; set; }
     public DbSet<User> Users { get; set; }
 }
--- Models/Todo.cs
+++
@@ -1,8 +0,0 @@
-namespace BirthdayTracker.Models;
-
-public class Todo
-{
-    public int Id { get; set; }
-    public string Title { get; set; } = "";
-    public bool IsCompleted { get; set; }
-}
